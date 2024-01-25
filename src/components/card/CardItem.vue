fa5a8f7502beaddbef8128cb4ffb8dc1f19020ab
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-25 21:22:40 +0100

    создал CardItem.vue для карточки

---
+++ src/components/card/CardItem.vue
@@ -0,0 +1,24 @@
+<template>
+	<fwb-card href="#">
+		<div class="flex items-center">
+			<!-- TODO: сделать карточку -->
+			<div>картинка</div>
+			<div class="p-5">
+				<h5
+					class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+				>
+					Алтын 739
+				</h5>
+				<p class="font-normal text-gray-700 dark:text-gray-400">
+					Для производства зерна и силоса.
+				</p>
+				<p class="font-normal text-gray-700 dark:text-gray-400">25 кг.</p>
+			</div>
+			<div>цена и кнопка</div>
+		</div>
+	</fwb-card>
+</template>
+
+<script setup>
+import { FwbCard } from 'flowbite-vue'
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,13 @@
+<script setup>
+import TheNavBar from '@/components/TheNavBar.vue'
+import CardItem from '@/components/card/CardItem.vue'
+</script>
+
 <template>
+	<TheNavBar />
 	<h1>Все товары</h1>
+	<div>фильтр checkbox с производителями</div>
+	<CardItem />
 </template>
 
 <style></style>
